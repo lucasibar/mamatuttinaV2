9471c661becddd235d1dd20c75955859e41bf8ed
Lucas Ibar <106266661+lucasibar@users.noreply.github.com>
2024-05-21 12:48:23 -0300

    funcionalidad a botones camnbio de dia

--- mamatuttinav2/src/components/Home/DayPagination.js
+++ mamatuttinav2/src/components/Home/DayPagination.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {dayBefore1, getDiary} from '../../redux/actions'
+import {dayBefore, dayAfter, getDiary} from '../../redux/actions'
 
 function DayPagination() {
    let dispatch = useDispatch()
    const dayNumber = useSelector((state) => state.day.number);
 
    const  dayBeforeButton = function(){
-      dispatch(dayBefore1(dayNumber))
+      dispatch(dayBefore(dayNumber))
+   }
+      const  dayAfterBotton = function(){
+      dispatch(dayAfter(dayNumber))
    }
 
    return (
       <div className='dayChange'>
-        <button className='dayBeforeBotton' onClick={dayBeforeButton}>{'<'}</button>
+        <button className='changeDayBotton' onClick={dayBeforeButton}>{'<'}</button>
         <p className='titleDay'>{dayNumber}</p>
-        <button className='dayAfterBotton'>{'>'}</button>
+        <button className='changeDayBotton' onClick={dayAfterBotton}>{'>'}</button>
       </div>
    )
 }
